9be3270645aa42d6835058fff04acb369291dfb4
akshath2704-ux <akshath2704@gmail.com>
2026-03-18 18:43:49 +0800

    initial commit

--- game biulding cocepts/activity3.py
+++ game biulding cocepts/activity3.py
@@ -1,6 +1,6 @@
 import pygame
-pygame.init()
 def main():
+    pygame.init()
     screen_width,screen_height=500,500
     screen=pygame.display.set_mode((screen_width,screen_height))
     pygame.display.set_caption('color changing sprite')
@@ -34,10 +34,10 @@ def main():
             current_color=colors['green']
         else:
             current_color=colors['white']
-            screen.fill((0,0,0))
-            pygame.draw.rect(screen,current_color,(x,y,sprite_width,sprite_height))
-            pygame.display.flip()
-            clock.tick(90)
-        pygame.quit()
+        screen.fill((0,0,0))
+        pygame.draw.rect(screen,current_color,(x,y,sprite_width,sprite_height))
+        pygame.display.flip()
+        clock.tick(90)
+    pygame.quit()
 if __name__=="__main__":
     main()
